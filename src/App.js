ab41406a23a22ef4c1f7d31a90c2179c3f052820
larryQuao <honyadjilawrence2s1@gmail.com>
2024-12-21 15:34:59 +0000

    Learning in Progress

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+let output = console;
+const someData = [
+  {
+    id: "e1",
+    title: "Air Conditioner",
+    amount: 124.12,
+    date: new Date(2024, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "Gaming PC",
+    amount: 799.49,
+    date: new Date(2023, 4, 12),
+  },
+  {
+    id: "e3",
+    title: "Bugatti",
+    amount: 2247.67,
+    date: new Date(2024, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "King Size Bed (Gold)",
+    amount: 450.09,
+    date: new Date(2024, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(someData);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    } );
+  };
+
+  return (
+    <div className="App">
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
