cd1457b95a3546ad6ac6e60e6820d8db88498603
larryQuao <honyadjilawrence2s1@gmail.com>
2024-12-29 23:09:40 +0000

    done

--- src/App.js
+++ src/App.js
@@ -3,31 +3,30 @@ import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-let output = console;
 const someData = [
   {
     id: "e1",
     title: "Air Conditioner",
     amount: 124.12,
-    date: new Date(2024, 7, 14),
+    date: new Date(2022, 7, 14),
   },
   {
     id: "e2",
     title: "Gaming PC",
     amount: 799.49,
-    date: new Date(2023, 4, 12),
+    date: new Date(2020, 4, 12),
   },
   {
     id: "e3",
     title: "Bugatti",
     amount: 2247.67,
-    date: new Date(2024, 2, 28),
+    date: new Date(2020, 2, 28),
   },
   {
     id: "e4",
     title: "King Size Bed (Gold)",
     amount: 450.09,
-    date: new Date(2024, 5, 12),
+    date: new Date(2019, 5, 12),
   },
 ];
 
@@ -36,9 +35,9 @@ const App = () => {
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses]
-    } );
-  };
+      return [expense, ...prevExpenses];
+    });
+  }; 
 
   return (
     <div className="App">
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,41 @@
-import "./NewExpense.css";
-import ExpenseForm from "./ExpenseForm";
+import React, { useState } from 'react';
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
-      id: Math.random.toString(),
       ...enteredExpenseData,
+      id: Math.random().toString()
     };
-    props.onAddExpense(expenseData)
-    // console.log(expenseData);
+    props.onAddExpense(expenseData);
+    setIsFormVisible(false); // Hide the form after submission
+  };
+
+  const showFormHandler = () => {
+    setIsFormVisible(true);
+  };
+
+  const hideFormHandler = () => {
+    setIsFormVisible(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isFormVisible && (
+        <button onClick={showFormHandler}>Add New Expense</button>
+      )}
+      {isFormVisible && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={hideFormHandler} // Pass a function to hide the form
+        />
+      )}
     </div>
   );
 };
 
 export default NewExpense;
+
